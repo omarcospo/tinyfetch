45d8a31f1f82600ab77da7a508c7edd41780405a
Marcos Felipe <marcos.felipe@usp.br>
2024-06-18 21:16:57 -0300

    divide function into smaller functions with error

--- main.go
+++ main.go
@@ -25,30 +25,40 @@ import (
 	"strings"
 )
 
-var (
+const (
 	reset string = "\033[0m"
 	red   string = "\033[31m"
 )
 
-func main() {
-	get_info("/etc/os-release", "NAME=", "Distro:")
-	get_env("USER", "User:")
-	get_env("SHELL", "Shell:")
-	get_info("/proc/cpuinfo", "model name\t:", "CPU:")
-	get_memory()
-	get_uptime()
-	get_env("LANG", "Locale:")
-}
-
-func errDo(err error, tag string) {
+func ReadInfoFile(path string, tag string) string {
+	file, err := os.ReadFile(path)
 	if err != nil {
 		errMsg := errors.New("\n" + red + "---> " + tag + "INFO NOT FOUND" + reset)
 		fmt.Println(errMsg)
 		os.Exit(1)
 	}
+	return string(file)
 }
 
-func get_env(env string, tag string) {
+func CutStrPrefix(content string, prefix string) string {
+	lines := strings.Split(content, "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(line, prefix) {
+			info := strings.TrimPrefix(line, prefix)
+			info = strings.Trim(strings.Trim(info, "\""), "  ")
+			return info
+		}
+	}
+	return ""
+}
+
+func GetInfo(path string, prefix string, tag string) {
+	file := ReadInfoFile(path, tag)
+	info := CutStrPrefix(file, prefix)
+	fmt.Println(red+tag+reset, info)
+}
+
+func GetEnv(env string, tag string) {
 	var envCmd string = os.Getenv(env)
 	if envCmd == "" {
 		errMsg := errors.New("\n" + red + "---> " + tag + "INFO NOT FOUND" + reset)
@@ -58,14 +68,10 @@ func get_env(env string, tag string) {
 	fmt.Println(red+tag+reset, envCmd)
 }
 
-func get_uptime() {
+func GetUptime() {
 	var tag string = "Uptime:"
-	file, err := os.ReadFile("/proc/uptime")
-	if err != nil {
-		fmt.Println("\n" + red + "---> " + "Uptime:" + "INFO NOT FOUND" + reset)
-		os.Exit(1)
-	}
-	info, _ := strconv.ParseFloat(strings.Split(string(file), " ")[0], 64)
+	var file = ReadInfoFile("/proc/uptime", tag)
+	info, _ := strconv.ParseFloat(strings.Split(file, " ")[0], 64)
 	if info >= 3600 {
 		fmt.Printf("%s%s%s %.0fh\n", red, tag, reset, info/3600)
 	} else {
@@ -74,38 +80,24 @@ func get_uptime() {
 	return
 }
 
-func get_info(path string, prefix string, tag string) {
-	file, err := os.ReadFile(path)
-	errDo(err, tag)
-	content := string(file)
-	lines := strings.Split(content, "\n")
-	for _, line := range lines {
-		if strings.HasPrefix(line, prefix) {
-			info := strings.TrimPrefix(line, prefix)
-			info = strings.Trim(info, "\"")
-			info = strings.Trim(info, "  ")
-			fmt.Println(red+tag+reset, info)
-			return
-		}
+func GetMemory() {
+	var tag string = "Memory:"
+	var prefix string = "MemTotal:"
+	var file = ReadInfoFile("/proc/meminfo", tag)
+	for _, line := range strings.Split(file, "\n") {
+		var info = CutStrPrefix(line, prefix)
+		infoFloat, _ := strconv.ParseFloat(strings.Trim(info, " kB"), 64)
+		fmt.Printf("%s%s%s %.2fGB\n", red, tag, reset, (infoFloat/1024)/1024)
+		return
 	}
 }
 
-func get_memory() {
-	var prefix string = "MemTotal:"
-	var tag string = "Memory:"
-	file, err := os.ReadFile("/proc/meminfo")
-	errDo(err, tag)
-	content := string(file)
-	lines := strings.Split(content, "\n")
-	for _, line := range lines {
-		if strings.HasPrefix(line, prefix) {
-			info := strings.TrimPrefix(line, prefix)
-			info = strings.Trim(info, "\"")
-			info = strings.Trim(info, "  ")
-			info = strings.Trim(info, " kB")
-			infoFloat, _ := strconv.ParseFloat(info, 64)
-			fmt.Printf("%s%s%s %.2fGB\n", red, tag, reset, (infoFloat/1024)/1024)
-			return
-		}
-	}
+func main() {
+	GetInfo("/etc/os-release", "NAME=", "Distro:")
+	GetEnv("USER", "User:")
+	GetEnv("SHELL", "Shell:")
+	GetInfo("/proc/cpuinfo", "model name\t:", "CPU:")
+	GetMemory()
+	GetUptime()
+	GetEnv("LANG", "Locale:")
 }
